20c25217248d74e1481e7d90325cb2c92f0187d3
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-02-26 00:13:35 -0500

    test Speech API; working.

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native'
+import * as Speech from 'expo-speech'
 
-export default function App() {
+export default function App () {
+  const speak = () => {
+    let thingToSay = 'testing the speech feature'
+    Speech.speak(
+      `hey guys military department here. did you ever think about enlisting in the military?`
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Press to hear some words' onPress={speak} />
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +25,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})

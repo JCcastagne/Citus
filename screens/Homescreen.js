314c53facbd211b9f4956cba06f6e4cded05077b
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-02-26 14:08:30 -0500

    move Homescreen to separate screens folder

---
+++ screens/Homescreen.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { Button } from 'react-native'
+import * as Speech from 'expo-speech'
+
+export default function App () {
+  const speak = () => {
+    let thingToSay = 'testing the speech feature'
+    Speech.speak(thingToSay)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Press to hear some words' onPress={speak} />
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})

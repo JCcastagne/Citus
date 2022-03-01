4db07d7f1765fa97ca4e761b1021ae014954a3fc
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-03-01 01:45:23 -0500

    Add time converter function (seconds to HH/MM/SS) format

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -17,6 +17,37 @@ const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
 const visualizerScale = 1.25
 
+function timeConverter (seconds) {
+  /**
+   * Format seconds to HH/MM/SS.
+   *
+   * @seconds {seconds} seconds
+   * @return {xx:xx:xx} || {xx:xx} - Hours(HH) being shown depends if there is 1 hour or more in total.
+   *
+   * @powtac: https://stackoverflow.com/questions/6312993/javascript-seconds-to-time-string-with-format-hhmmss
+   **/
+  var sec_num = parseInt(minutes, 10)
+  var hours = Math.floor(sec_num / 3600)
+  var minutes = Math.floor((sec_num - hours * 3600) / 60)
+  var seconds = sec_num - hours * 3600 - minutes * 60
+
+  if (hours < 10) {
+    hours = '0' + hours
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+  if (seconds < 10) {
+    seconds = '0' + seconds
+  }
+
+  if (hours == 0) {
+    return minutes + ':' + seconds
+  } else {
+    return hours + ':' + minutes + ':' + seconds
+  }
+}
+
 export default function App ({ navigation }) {
   //Speech functionality
   function speak (thingToSay) {
@@ -78,7 +109,7 @@ export default function App ({ navigation }) {
             color: '#6DBDC9'
           }}
         >
-          15:00
+          {time}
         </Text>
         <ImageBackground
           id='visualizer'
